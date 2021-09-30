5cacb443e88d550514f94597d5fb4576ac99e39e
DevLomoSE <jonathancggs@gmail.com>
2021-09-30 11:10:35 -0500

    :recycle: Exports
    add the export declaration for the diferents modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 
 import { PrimeNGModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { PrimeNGModule } from './prime-ng/prime-ng.module';
   ],
   imports: [
     BrowserModule,
-    PrimeNGModule
+    AppRouterModule,
+    PrimeNGModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
