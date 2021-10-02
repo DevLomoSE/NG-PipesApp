bff8151097ad0f1c8a41c3cb06ce5915fa61842a
DevLomoSE <jonathancggs@gmail.com>
2021-10-02 14:14:10 -0500

    :sparkles: i18n Pipes
    adding the pipes for select and plural of i18n

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from '../../interfaces/persona.interface';
 
 @Component({
   selector: 'app-no-comunes',
@@ -8,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
-  constructor() { }
+  nombre!: string;
+  gender!: string;
+  
+  personas: Persona[] = [
+    {nombre:'jonathan', genero:'masculino'},
+    {nombre:'maria', genero:'femenino'},
+    {nombre:'pedro', genero:'masculino'},
+    {nombre:'juan', genero:'masculino'},
+    {nombre:'martha', genero:'femenino'},
+    {nombre:'alicia', genero:'femenino'},
+    {nombre:'dilton', genero:'masculino'},
+  ];
+  invitacionMap = {
+    'masculino':'invitarlo',
+    'femenino':'invitarla',
+    'other':this.nombre
+  };
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  };
+
+  randomIndex!: number;
+  MIN: number = 1;
+
+  constructor() {
+    this.randomIndex = this.getRandomIndex();
+    this.getPersona();
+  }
 
   ngOnInit(): void {
+  }
+
+  reload(): void{
+    this.randomIndex = this.getRandomIndex();
+    this.getPersona();
+  }
+
+  getRandomIndex(): number{
+    return Math.floor(Math.random()*(this.personas.length - this.MIN)+this.MIN);
+  }
+
+  getPersona(): void{
+    let persona: Persona = this.personas[(this.randomIndex < this.personas.length) 
+                                          ? this.randomIndex 
+                                          : 0];
+    this.nombre = (persona) ? persona.nombre:'';
+    this.gender = (persona) ? persona.genero:'';
+  }
+
+  deletePersona(): void{
+    this.personas.splice((this.randomIndex < this.personas.length) 
+                                          ? this.randomIndex 
+                                          : 0,1);
+    this.reload();
   }
 
 }
