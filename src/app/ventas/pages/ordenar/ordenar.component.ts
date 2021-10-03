a7553aa028cc0f0fde028b3f2cfefa0198cc446e
DevLomoSE <jonathancggs@gmail.com>
2021-10-03 12:45:13 -0500

    :sparkles: ordenar
    adding functionality to order data using primeng and custom pipes

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenarComponent implements OnInit {
 
-  constructor() { }
+  enMayusculas: boolean = true;
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: true,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ];
+
+  orderBy!: string;
+
+  constructor() {
+    this.orderBy = 'na';
+  }
 
   ngOnInit(): void {
+  }
+
+  changeOrderBy(orderBy: string){
+    this.orderBy = orderBy;
+  }
+
+  changeFlag(){
+    this.enMayusculas = !this.enMayusculas;
   }
 
 }
